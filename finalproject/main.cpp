ee32a0dabd4e607093b6e48decee69f596d6ed11
thenujah.magendran <thenu.m@hotmail.com>
2013-11-15 14:50:41 -0500

    example set up

---
+++ finalproject/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: tmagendr
+ */
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	int x, y, total;
+	x = 10;
+	y = 15;
+	total = x + y ;
+
+	cout << "The sum of is:" << total;
+}
+
